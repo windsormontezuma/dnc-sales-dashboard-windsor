ffd992212e0225e9f48b1eaf3059b2ffc3d47f1e
dev.montezuma <163691300+windsormontezuma@users.noreply.github.com>
2025-11-14 13:02:51 -0300

    Feat: Create Themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import ReactDom from 'react-dom/client'
 import App from './App.tsx'
+import { GlobalStyle, lightTheme } from './styles/'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDom.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 )
